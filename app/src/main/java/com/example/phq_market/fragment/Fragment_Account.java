f1da3d860bc80b06911cec8792c096c6fc042045
224pt304 <127222828+224pt304@users.noreply.github.com>
2023-11-01 23:47:23 +0700

    test thử nào

---
+++ app/src/main/java/com/example/phq_market/fragment/Fragment_Account.java
@@ -0,0 +1,34 @@
+package com.example.phq_market.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.phq_market.R;
+
+
+public class Fragment_Account extends Fragment {
+
+
+
+    public Fragment_Account() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment__account, container, false);
+    }
+}
