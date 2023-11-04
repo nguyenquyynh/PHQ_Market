c9b508c4cae36153a98f7d40c261264ebbf50628
nguyenquyynh <129946650+nguyenquyynh@users.noreply.github.com>
2023-11-04 16:13:30 +0700

    abc

--- app/src/main/java/com/example/phq_market/model/NEWPRODUCT.java
+++ app/src/main/java/com/example/phq_market/model/NEWPRODUCT.java
@@ -5,12 +5,15 @@ public class NEWPRODUCT {
     private String NAME;
     private Float PRICE;
     private Double EVALUATE;
+    private String IMG;
 
-    public NEWPRODUCT(Integer ID, String NAME, Float PRICE, Double EVALUATE) {
+    public NEWPRODUCT(Integer ID, String NAME, Float PRICE, Double EVALUATE, String IMG
+    ) {
         this.ID = ID;
         this.NAME = NAME;
         this.PRICE = PRICE;
         this.EVALUATE = EVALUATE;
+        this.IMG = IMG;
     }
 
     public NEWPRODUCT() {
@@ -46,5 +49,13 @@ public class NEWPRODUCT {
 
     public void setEVALUATE(Double EVALUATE) {
         this.EVALUATE = EVALUATE;
+    }
+
+    public String getIMG() {
+        return IMG;
+    }
+
+    public void setIMG(String IMG) {
+        this.IMG = IMG;
     }
 }
